b527da4c68efc00ce6727e4af1870da6a23ff79d
Tommy White <twhite306@gmail.com>
2025-01-20 15:12:15 +0000

    REFACTOR: Refactor recipeController to correctly identify empty search queries

--- server/src/controllers/recipeController.ts
+++ server/src/controllers/recipeController.ts
@@ -1,20 +1,24 @@
-'use strict';
-import Recipe from '../models/recipe';
-import { Request, Response } from 'express';
+"use strict";
+import Recipe from "../models/recipe";
+import { Request, Response } from "express";
 
 const getRecipes = async (req: Request, res: Response): Promise<any> => {
   try {
-    if (!req.query) {
+    if (Object.keys(!req.query).length === 0) {
       const recipes = await Recipe.find();
       return res.send(recipes);
     } else {
       const searchQuery = req.query.q as string;
-      const recipes = await Recipe.find({$text: {$search: searchQuery}});
+      const recipes = await Recipe.find({ $text: { $search: searchQuery } });
       return res.send(recipes);
     }
   } catch (e) {
+    console.log("Req query: ", req.query);
     console.log(e);
-    return res.status(500).send({error: {message: 'Error getting recipes!', code: 500}});
+
+    return res
+      .status(500)
+      .send({ error: { message: "Error getting recipes!", code: 500 } });
   }
 };
 
@@ -22,37 +26,53 @@ const getRecipe = async (req: Request, res: Response): Promise<any> => {
   try {
     const id = req.params.recipeId;
     if (!id) {
-      return res.status(400).send({error: {message: 'No recipe id provided!', code: 400}});
+      return res
+        .status(400)
+        .send({ error: { message: "No recipe id provided!", code: 400 } });
     }
-    const recipe = await Recipe.findOne({_id: id});
+    const recipe = await Recipe.findOne({ _id: id });
     return res.send(recipe);
   } catch (e) {
     console.log(e);
-    return res.status(500).send({error: {message: 'Error getting recipe!', code: 500}});
+    return res
+      .status(500)
+      .send({ error: { message: "Error getting recipe!", code: 500 } });
   }
 };
 
-const getRecipesByCategory = async (req: Request, res: Response): Promise<any> => {
+const getRecipesByCategory = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
   try {
     const category = req.params.category;
     if (!category) {
-      return res.status(400).send({error: {message: 'No category provided!', code: 400}});
+      return res
+        .status(400)
+        .send({ error: { message: "No category provided!", code: 400 } });
     }
-    const recipes = await Recipe.find({category});
+    const recipes = await Recipe.find({ category });
     return res.send(recipes);
   } catch (e) {
     console.log(e);
-    return res.status(500).send({error: {message: 'Error getting recipes!', code: 500}});
+    return res
+      .status(500)
+      .send({ error: { message: "Error getting recipes!", code: 500 } });
   }
 };
 
-const getLastAddedRecipes = async (req: Request, res: Response): Promise<any> => {
+const getLastAddedRecipes = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
   try {
     const recipes = await Recipe.find().sort({ createdAt: -1 }).limit(10);
     return res.send(recipes);
   } catch (e) {
     console.log(e);
-    return res.status(500).send({error: {message: 'Error getting recipes!', code: 500}});
+    return res
+      .status(500)
+      .send({ error: { message: "Error getting recipes!", code: 500 } });
   }
 };
 
@@ -60,41 +80,62 @@ const postRecipe = async (req: Request, res: Response): Promise<any> => {
   try {
     const body = req.body;
     if (!body) {
-      return res.status(400).send({error: {message: 'Request body missing!', code: 400}});
+      return res
+        .status(400)
+        .send({ error: { message: "Request body missing!", code: 400 } });
     }
     const recipe = await Recipe.create(body);
     return res.status(201).send(recipe);
   } catch (e) {
     console.log(e);
-    return res.status(500).send({error: {message: 'Error creating recipe!', code: 500}});
+    return res
+      .status(500)
+      .send({ error: { message: "Error creating recipe!", code: 500 } });
   }
 };
 
 const postReview = async (req: Request, res: Response): Promise<any> => {
   try {
-    const {rating, message} = req.body;
+    const { rating, message } = req.body;
     if (!rating && !message) {
-      return res.status(400).send({error: {message: 'Missing rating or review!', code: 400}});
+      return res
+        .status(400)
+        .send({ error: { message: "Missing rating or review!", code: 400 } });
     }
     const id = req.params.recipeId;
     if (!id) {
-      return res.status(400).send({error: {message: 'No recipe id provided!', code: 400}});
+      return res
+        .status(400)
+        .send({ error: { message: "No recipe id provided!", code: 400 } });
     }
-    const recipe = await Recipe.findOne({_id: id});
+    const recipe = await Recipe.findOne({ _id: id });
     if (!recipe) {
-      return res.status(404).send({ error: { message: 'Recipe not found!', code: 404 } });
+      return res
+        .status(404)
+        .send({ error: { message: "Recipe not found!", code: 404 } });
     }
     const oldRating = recipe.rating;
 
     recipe.reviews.push(req.body);
-    const newRating = parseFloat(((oldRating + rating)/recipe.reviews.length).toFixed(2));
+    const newRating = parseFloat(
+      ((oldRating + rating) / recipe.reviews.length).toFixed(2)
+    );
     recipe.rating = newRating;
     await recipe.save();
     return res.status(200).send(req.body);
   } catch (e) {
     console.log(e);
-    return res.status(500).send({error: {message: 'Error creating recipe!', code: 500}});
+    return res
+      .status(500)
+      .send({ error: { message: "Error creating recipe!", code: 500 } });
   }
 };
 
-export {getRecipes, getRecipe, getRecipesByCategory, getLastAddedRecipes, postRecipe, postReview};
+export {
+  getRecipes,
+  getRecipe,
+  getRecipesByCategory,
+  getLastAddedRecipes,
+  postRecipe,
+  postReview,
+};

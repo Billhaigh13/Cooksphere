5ac57941a05f86c7695443b1174cf5b8f8053e3e
Tommy White <twhite306@gmail.com>
2025-01-21 15:07:08 +0000

    REFACTOR: Some upload components converted to TS

--- client/src/components/uploadpage/Ingredient.tsx
+++ client/src/components/uploadpage/Ingredient.tsx
@@ -1,12 +1,32 @@
 import { Input } from "../common/Input";
 
-export function Ingredient ({number, values, handleChange}) {
+interface IngredientProps {
+  number: number;
+  values: Record<string, string>;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export function Ingredient({ number, values, handleChange }: IngredientProps) {
   return (
     <>
-      <div className="flex gap-4">
-        <Input id={'ingredient-' + number} name={'ingredient-' + number} value={values['ingredient'+number]} text={'Ingredient ' + number + ':'} handleChange={handleChange}/>
-        <Input id={'measure-' + number} name={'measure-' + number} value={values['measure'+number]} text={'Measure ' + number + ':'} handleChange={handleChange}/>
+      <div className='flex gap-4'>
+        <Input
+          id={"ingredient-" + number}
+          name={"ingredient-" + number}
+          value={values["ingredient" + number]}
+          text={"Ingredient " + number + ":"}
+          handleChange={handleChange}
+          error=''
+        />
+        <Input
+          id={"measure-" + number}
+          name={"measure-" + number}
+          value={values["measure" + number]}
+          text={"Measure " + number + ":"}
+          handleChange={handleChange}
+          error=''
+        />
       </div>
     </>
   );
-};
+}

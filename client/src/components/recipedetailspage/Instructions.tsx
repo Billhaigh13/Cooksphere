fc068140d0fa204630453af1a1c53347a614288f
Tommy White <twhite306@gmail.com>
2025-01-21 14:54:16 +0000

    REFACTOR: Reviews bug fixes

--- client/src/components/recipedetailspage/Instructions.tsx
+++ client/src/components/recipedetailspage/Instructions.tsx
@@ -1,15 +1,24 @@
-export function Instructions ({instructions}) {
+interface InstructionProps {
+  instructions: string[];
+}
+export function Instructions({ instructions }: InstructionProps) {
   return (
     <>
       <div className='p-4 bg-brown rounded-xl shadow_2'>
-        <h2 className='text-xl font-semibold flex items-center gap-2 text-white px-4'>Instructions
-          <img src="/spoon.svg" alt="spoon-icon" className='w-5 h-5'/>
+        <h2 className='text-xl font-semibold flex items-center gap-2 text-white px-4'>
+          Instructions
+          <img src='/spoon.svg' alt='spoon-icon' className='w-5 h-5' />
         </h2>
         <ol className='p-4 flex flex-col gap-4'>
           {instructions.map((instruction, index) => (
             <>
-              <li key={index} className='p-2 rounded-md bg-lightbeige flex flex-col gap-2'>
-                <div className='h-fit w-[3.75rem] text-center rounded-md bg-softyellow text-deepbrown font-poppins uppercase text-sm'>{'Step ' + (index+1)}</div>
+              <li
+                key={index}
+                className='p-2 rounded-md bg-lightbeige flex flex-col gap-2'
+              >
+                <div className='h-fit w-[3.75rem] text-center rounded-md bg-softyellow text-deepbrown font-poppins uppercase text-sm'>
+                  {"Step " + (index + 1)}
+                </div>
                 {instruction}
               </li>
             </>
@@ -17,5 +26,5 @@ export function Instructions ({instructions}) {
         </ol>
       </div>
     </>
-  )
+  );
 }

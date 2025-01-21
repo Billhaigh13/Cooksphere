63904d6f32638ab814268a0279d5bbb001cf522c
bill haigh <williamhaigh297@gmail.com>
2025-01-21 11:14:03 +0000

    REFACTOR: utils/sortRecipes.ts complete

--- client/src/utils/sortRecipes.ts
+++ client/src/utils/sortRecipes.ts
@@ -1,12 +1,14 @@
-export function sortRecipes(recipes, sortBy) {
+import { Recipe } from "../types/types";
+
+export function sortRecipes(recipes: Recipe[], sortBy: string): Recipe[] {
   if (sortBy === "A-Z") {
     return [...recipes].sort((a, b) => (a.name > b.name ? 1 : -1));
   } else if (sortBy === "Z-A") {
     return [...recipes].sort((a, b) => (a.name < b.name ? 1 : -1));
   } else if (sortBy.includes("Best")) {
-    return [...recipes].sort((a, b) => a.rating < b.rating);
+    return [...recipes].sort((a, b) => a.rating < b.rating ? 1 : -1);
   } else if (sortBy.includes("Worst")) {
-    return [...recipes].sort((a, b) => a.rating > b.rating);
+    return [...recipes].sort((a, b) => a.rating > b.rating ? 1 : -1);
   }
   return recipes;
 }

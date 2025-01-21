29e5d5e79a4ba155ae6bc7fbf95bbc34054d44c3
Tommy White <twhite306@gmail.com>
2025-01-21 11:45:21 +0000

    REFACTOR: intermediates/SortSelect converted to TS

--- client/src/components/intermediates/SortSelect.tsx
+++ client/src/components/intermediates/SortSelect.tsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+interface SortProps {
+  setSorting: (sorting: string) => void;
+}
 
-export function SortSelect ({setSorting}) {
+export function SortSelect({ setSorting }: SortProps) {
   return (
     <>
       <div className='mb-4 px-'>
-        <label htmlFor="sort" className='text-lg'>Sort recipes by:</label>
-        <select name="sort" id="sort" className='px-2 py-2 rounded-lg ml-4 cursor-pointer bg-softyellow' onChange={(event) => setSorting(event.target.value)}>
-          <option disabled selected value hidden>-- Select an option --</option>
-          <option value="A-Z">A-Z</option>
-          <option value="Z-A">Z-A</option>
-          <option value="Best recipes first">Best recipes first</option>
-          <option value="Worst recipes first">Worst recipes first</option>
+        <label htmlFor='sort' className='text-lg'>
+          Sort recipes by:
+        </label>
+        <select
+          name='sort'
+          id='sort'
+          className='px-2 py-2 rounded-lg ml-4 cursor-pointer bg-softyellow'
+          onChange={(event) => setSorting(event.target.value)}
+        >
+          <option disabled selected value='' hidden>
+            -- Select an option --
+          </option>
+          <option value='A-Z'>A-Z</option>
+          <option value='Z-A'>Z-A</option>
+          <option value='Best recipes first'>Best recipes first</option>
+          <option value='Worst recipes first'>Worst recipes first</option>
         </select>
       </div>
     </>

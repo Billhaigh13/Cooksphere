537071a3eb81a92d7b62464ff35c412000bb5179
bill haigh <williamhaigh297@gmail.com>
2025-01-21 14:07:04 +0000

    REFACTOR: uploadpage/instruction.tsx complete

--- client/src/components/uploadpage/Instruction.tsx
+++ client/src/components/uploadpage/Instruction.tsx
@@ -1,10 +1,12 @@
-export function Instruction ({number, handleChange}) {
+import { ChangeEventHandler } from "react";
+
+export function Instruction ({number, handleChange}: {number: number, handleChange: ChangeEventHandler<HTMLTextAreaElement>}) {
 
   return (
     <>
       <div className="flex flex-col gap-1">
         <label htmlFor={'instruction-' + number} className="text-white leading-4 text-sm">{'Step ' + number}</label>
-        <textarea id={'instruction-' + number} name={'instruction-' + number} rows="3" className="outline-none rounded-md px-1" onChange={handleChange}>
+        <textarea id={'instruction-' + number} name={'instruction-' + number} rows={3} className="outline-none rounded-md px-1" onChange={handleChange}>
         </textarea>
       </div>
     </>

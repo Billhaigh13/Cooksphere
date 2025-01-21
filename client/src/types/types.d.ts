9e4a3fcad41890d7c1a7d6e8ba80c97192e864ed
Tommy White <twhite306@gmail.com>
2025-01-21 09:46:16 +0000

    REFACTOR: Types file init and initial conversions to TypeScript

---
+++ client/src/types/types.d.ts
@@ -0,0 +1,31 @@
+export type Category = {
+  name: string;
+  image: string;
+};
+
+export type Ingredient = {
+  ingredient: string;
+  measure: string;
+};
+
+export type Recipe = {
+  name: string;
+  category: string;
+  instructions: string[];
+  image: string;
+  tags: string[];
+  ingredients: Ingredient[];
+  cookingTimeInMinutes: number;
+  rating: number;
+  reviews: string[];
+};
+
+export type User = {
+  firstname: string;
+  lastname: string;
+  image: string;
+  email: string;
+  password: string;
+  favoriteRecipes: Recipe[];
+  uploadedRecipes: Recipe[];
+};
